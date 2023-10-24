9fce9efc7f155fbbd2247f0f6c95887268894620
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-24 15:50:13 -0700

    completed typewriter

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+let n = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, n)
+
+  n += 50;
+}
+
+setTimeout(() => {
+  console.log();
+}, n)
